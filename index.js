fe448cc3b852e0f938ee6e86d1de202f3784dd4f
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-19 18:09:18 -0700

    Add 3 options to keep context in an inner function

--- index.js
+++ index.js
@@ -10,3 +10,50 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+//! here thisArg (this) is passed into the forEach as another argument
+const printCardThisArg = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  this.signatories.forEach(function (signatory) {
+    const message = `${this.closing[signatory]}, ${signatory}`
+    console.log(message);
+  }, this);
+};
+
+//! something similar would be to bind content 
+// const contextBound = function (signatory) {
+//   do stuff
+// }.bind(this);
+// this.signatories.forEach(contextBound);
+
+// printCardThisArg.call(messageConfig);
+
+//! here you can save this as a variable and so that you can now access it as part of function level scope 
+
+const printCardClosure = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  const outerContext = this;
+
+  this.signatories.forEach(function (signatory) {
+    const message = `${outerContext.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  });
+};
+
+// printCardClosure.call(messageConfig);
+
+//! here you can use an arrow function to absorb the parent's context
+
+const printCardArrowFunction = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCardArrowFunction.call(messageConfig);
